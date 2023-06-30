11e760c978461e2f8bfb2d2469c8ef7454d7485f
Dyan <lumbar_plaice0i@icloud.com>
2023-06-30 14:21:24 -0300

    feat: Control Height of Views with GeometryReader

--- Chat/ChatView.swift
+++ Chat/ChatView.swift
@@ -11,6 +11,8 @@ struct ChatView: View {
     let contact: Contact
     @StateObject var viewModel = ChatViewModel()
     
+    @State var textSize: CGSize = .zero
+    
     var body: some View {
         VStack{
             ScrollView(showsIndicators: false) {
@@ -22,16 +24,33 @@ struct ChatView: View {
             Spacer()
             
             HStack {
-                TextField("Enter a mensagem", text: $viewModel.text)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(24)
-                    .overlay (
-                        RoundedRectangle(cornerRadius: 24)
-                            .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1))
-                    )
+                ZStack {
+                    TextEditor(text: $viewModel.text)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(24)
+                        .overlay (
+                            RoundedRectangle(cornerRadius: 24)
+                                .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1))
+                        )
+                        .frame(maxHeight: (textSize.height + 50) > 100 ? 100 : textSize.height + 50)
+                    
+                    Text(viewModel.text)
+                        .opacity(0)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(ViewGeometry())
+                        .lineLimit(4)
+                        .multilineTextAlignment(.leading)
+                        .padding(.horizontal, 21)
+                        .onPreferenceChange(ViewSizeKey.self) { size in
+                            print("textSize is \(size)")
+                            textSize = size
+                        }
+                    
+                }
+                
                 Button {
                     viewModel.sendMessage(contact: contact)
                 } label: {
@@ -51,6 +70,23 @@ struct ChatView: View {
         .onAppear{
             viewModel.onAppear(contact: contact)
         }
+    }
+}
+
+struct ViewGeometry: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Color.clear.preference(key: ViewSizeKey.self, value: geometry.size)
+        }
+    }
+}
+
+struct ViewSizeKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        print("new value is \(value)")
+        value = nextValue()
     }
 }
 
